deac9a8fe8f7384ecc77dfc61415cb6cc9a4dd56
root <root@localhost.localdomain>
2021-01-31 19:54:43 +0000

    1612122880487

--- index.js
+++ index.js
@@ -2,68 +2,97 @@ const express = require('express')
 const app = express()
 const port = 8081
 const { exec } = require('child_process')
+let today = Date.now()
 
-function cding() {
-    return exec("cd /opt/FacilContabilidad-API", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    }) 
+const commands = {
+    cd: () => {
+        return exec("cd /opt/FacilContabilidad-API", (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        }) 
+    },
+    stopAPI: () => {
+        return exec("pm2 stop api", (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        }) 
+    },
+    startAPI: () => {
+        return exec("pm2 stop api", (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        }) 
+    },
+    gitAdd: () => {
+        return exec("git add .", (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        })      
+    },
+    gitCommit: () => {
+        return exec(`git commit -m ${today}`, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        })     
+    },
+    gitPulling: () => {
+        return exec("git pull", (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        }) 
+    }
 }
 
-function stopAPI() {
-    return exec("pm2 stop api", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    }) 
-}
-
-function startAPI() {
-    return exec("pm2 stop api", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    }) 
-}
-
-function gitPulling() {
-    return exec("git pull", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    }) 
-}
-
-app.post('/', (req, res) => {
-    console.log(req)
-    cding()
-    gitPulling()
-
+app.get('/', (req, res) => {
+  
+    commands.stopAPI()
+    commands.cd()
+    commands.gitAdd()
+    commands.gitCommit()
+    commands.gitPulling()
+    commands.startAPI()
 })
 
 app.listen(port, () => {
