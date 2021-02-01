c405f894327b26f5326c45fef2177882847b49bd
Miguel Suárez <suarezmiguelc23@gmail.com>
2021-02-01 04:00:14 -0500

    status

--- index.js
+++ index.js
@@ -97,5 +97,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Examplee app listening at http://localhost:${port}`)
 })
